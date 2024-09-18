dd7296cf14a55b8b72056c634d863d0cfef38dbf
Jaymar Westerlow <jaywesterlow71@gmail.com>
2024-09-18 12:06:17 +0200

    basic styling, pseudo classes for borders as preperation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,12 +12,6 @@
 
 <main>
 	<section>
-		<article>
-			<h1>{data.persons.name} {data.persons.surname}</h1>
-
-			<p>{data.persons.bio}</p>
-		</article>
-
 		<aside>
 			<span class="avatar">
 				{#if data.persons.avatar.length > 0 && checkAvatarImage(data.persons.avatar)}
@@ -32,9 +26,69 @@
 				{/if}
 			</span>
 		</aside>
+
+		<article>
+				<h1>{data.persons.name} {data.persons.surname}</h1>
+				<p>{data.persons.bio}</p>
+		</article>
 	</section>
 </main>
 
 <style>
+  main {
+	width: 100%;
+	height: 100%;
+  }
+
+  main section {
+	width: 100%;
+	height: 100%;
+  }
+
+  main section aside {
+	position: relative;
+	z-index: 5;
+	width: calc(200px + 6rem);
+	height: calc(200px + 6rem);
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	background-color: var(--bg-color);
+  }
   
+  main section aside::after {
+	content: "";
+	position: absolute;
+	z-index: 4;
+	border: 2px solid var(--color-white);
+	width: calc(200px + 6rem);
+	height: calc(200px + 6rem);
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+  }
+  
+  main section aside span, main section aside span img {
+	width: 100%;
+  }
+  
+  main section article {
+	border: 2px solid var(--color-white);
+	width: 200px;
+	height: 200px;
+	padding: 3rem;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	text-align: center;
+	font-family: "montserrat";
+	background-color: var(--bg-color);
+  }
+  
+  main section article h1, main section article p {
+	color: var(--color-white);
+	margin: .75rem 0 .75rem 0;
+  }
+
+
 </style>
